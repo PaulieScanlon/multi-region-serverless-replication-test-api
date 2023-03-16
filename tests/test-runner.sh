bc89f30a8a746532646a8f5038942f371a76d203
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-16 12:54:01 +0000

    feat: add read-buildings function

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -1,1 +1,4 @@
 serverless invoke local --function create-building --path ./tests/create-building.json
+
+
+serverless invoke local --function read-buildings
---
+++ v1/read-buildings.js
@@ -0,0 +1,35 @@
+const { getDB } = require('../pg');
+
+module.exports.handler = async () => {
+  const client = await getDB().connect();
+
+  try {
+    const response = await client.query('SELECT * from buildings');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Read Buildings v1 - A Ok!',
+          region: process.env.AWS_REGION,
+          data: {
+            buildings: response.rows || [],
+          },
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        { message: 'Read Buildings v2 - Error!', region: process.env.AWS_REGION, error: error },
+        null,
+        2
+      ),
+    };
+  } finally {
+    client.release();
+  }
+};
