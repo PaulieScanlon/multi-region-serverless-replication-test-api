35b619a183359675f99ba741afb157869fb87ce1
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-16 15:16:54 +0000

    chore: add test function using queryParameters

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -1,4 +1,5 @@
 serverless invoke local --function create-building --path ./tests/create-building.json
 
-
 serverless invoke local --function read-buildings
+
+serverless invoke local --function test --path ./tests/test.json
---
+++ v1/test.js
@@ -0,0 +1,26 @@
+module.exports.handler = async (event, context) => {
+  const { type, color } = event.queryStringParameters;
+
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Test v1 - A Ok!',
+          data: {
+            type,
+            color,
+          },
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Test v1 - Error!', region: process.env.AWS_REGION, error: error }, null, 2),
+    };
+  } finally {
+  }
+};
